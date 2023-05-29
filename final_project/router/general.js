dd1e8208b1e4e18e5143ee7a62af700d0b0d78a9
Nwankwo Henry <nameisifunanya@gmail.com>
2023-05-29 11:00:40 +0100

    Created the logic to get book details by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,7 +45,21 @@ public_users.get('/isbn/:isbn', function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: 'Yet to be implemented' });
+  const author = req.params.author;
+  if (author) {
+    const filteredAuthor = Object.values(books).filter(
+      (book) => book.author.toLowerCase() == author.toLowerCase()
+    );
+    if (filteredAuthor.length > 0) {
+      return res.status(201).json(filteredAuthor);
+    } else {
+      return res
+        .status(201)
+        .json({ message: `There is no author with the name ${author} found!` });
+    }
+  } else {
+    return res.status(400).json({ message: 'There has been an error!' });
+  }
 });
 
 // Get all books based on title
